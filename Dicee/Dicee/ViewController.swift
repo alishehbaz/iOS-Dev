d077aa32315d0b98722a467d4d77763400b5e0c4
Ali Shehbaz <alishahbaz072@gmail.com>
2023-03-01 19:27:30 -0500

    dicee: initial project setup

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by IDM Student on 3/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
