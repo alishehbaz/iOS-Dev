5374b8f6120ffa7f7d0b6a0f79a0a2fcc0794423
Ali Shehbaz <alishahbaz072@gmail.com>
2023-03-01 19:27:29 -0500

    add initial setup of I am Batman

---
+++ I Am Batman/I Am Batman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Batman
+//
+//  Created by IDM Student on 3/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
