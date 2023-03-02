3c4be411a8580653a11f1d000f7d499a19d12883
Ali Shehbaz <alishahbaz072@gmail.com>
2023-03-01 19:27:30 -0500

    set up the connection

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -9,9 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
